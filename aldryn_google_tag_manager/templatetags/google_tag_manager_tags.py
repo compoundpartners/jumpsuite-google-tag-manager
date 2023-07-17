c417ddd8a3b2670752e8f7095996c84aec1c7dc6
evgeny-dmi3ev <evgeny.dmi3ev@gmail.com>
2023-07-17 12:05:22 +0200

    update

--- aldryn_google_tag_manager/templatetags/google_tag_manager_tags.py
+++ aldryn_google_tag_manager/templatetags/google_tag_manager_tags.py
@@ -10,19 +10,20 @@ register = Library()
 
 
 @register.inclusion_tag("aldryn_google_tag_manager/google_tag_manager.html")
-def google_tag_manager(tag_id=''):
-    if not tag_id and hasattr(settings, 'GOOGLE_TAG_MANAGER_ID'):
-        tag_id = settings.GOOGLE_TAG_MANAGER_ID
-    return {
-        'tag_id': tag_id
-    }
+def google_tag_manager(*args, **kwargs):
+    if len(args) == 1 and args[0]:
+        kwargs['tag_id'] = args[0]
+    elif not 'tag_id' in kwargs and hasattr(settings, 'GOOGLE_TAG_MANAGER_ID'):
+        kwargs['tag_id'] = settings.GOOGLE_TAG_MANAGER_ID
+    return kwargs
 
 @register.simple_tag
 def data_layer(**kwargs):
     data = []
     for key, value in kwargs.items():
         if value:
-            data.append("'%s': '%s'" % (key.replace('_', '-'), value))
+            value = '%s' % value
+            data.append("'%s': '%s'" % (key.replace('_', '-'), value.replace('&amp;', '&')))
     return mark_safe('''<script>
     var dataLayer = window.dataLayer = window.dataLayer || [];
     dataLayer.push({
